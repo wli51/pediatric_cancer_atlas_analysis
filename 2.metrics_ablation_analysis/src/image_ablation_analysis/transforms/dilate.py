eccbbc6b673912be5445933b6e37c719e958afb7
Weishan Li <wli51@jh.edu>
2025-10-31 12:04:16 -0600

    Add dilate and erode transforms for image ablation analysis

---
+++ 2.metrics_ablation_analysis/src/image_ablation_analysis/transforms/dilate.py
@@ -0,0 +1,22 @@
+"""
+dilate.py
+
+Dilate transform for image ablation analysis.
+"""
+
+import cv2
+import numpy as np
+import albumentations as A
+
+
+class Dilate(A.ImageOnlyTransform):
+    def __init__(self, k: int = 3, iterations: int = 1, always_apply=False, p=1.0):
+        super().__init__(always_apply, p)
+        self.k, self.iterations = k | 1, iterations
+    def apply(self, img, **params):
+        kernel = np.ones((self.k, self.k), np.uint8)
+        return cv2.dilate(img, kernel, iterations=self.iterations)
+    def get_transform_init_args_names(self):
+        return ("k", "iterations")
+    def __repr__(self):
+        return f"Dilate(k={self.k}, iterations={self.iterations})"
